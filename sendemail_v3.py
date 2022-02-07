5dd7ced857a919712f71a19afa4b0158d73bbfdb
Nathan Tolene <nathant@utm.edu>
2022-02-07 15:14:35 -0600

    added one off to send

--- sendemail_v3.py
+++ sendemail_v3.py
@@ -76,6 +76,7 @@ def check_db_for_email_address(cat_id):
         name_to_email = x['name']
         one_off = x['one_off']
         email_id = x['ID']
+        print(email_id)
         print(one_off)
         if check_cat_id == cat_id:
             syslog.syslog('Email Address: ' + address_to_send)
