cac532e1838b9d6b4dc8e07e13c4ee48b4dfdbaf
Nathan Tolene <nathant@utm.edu>
2022-02-21 10:28:19 -0600

    checking for 404

--- v2_get_zoom_recordings.py
+++ v2_get_zoom_recordings.py
@@ -345,6 +345,7 @@ def delete_recordings_from_zoom():
                 mycursor.execute(select_sql)
                 mydb.commit()
             if check.status_code == '404':
+                print("Status code is 404 marking as downloaded")
                 select_sql = "update meetings set downloaded = 1 where meeting_id = '" + meeting_id + "'"
                 mycursor.execute(select_sql)
                 mydb.commit()
