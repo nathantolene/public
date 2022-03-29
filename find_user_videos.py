910921cc1b8db85720cc5609a2a364b239fd98ed
Nathan Tolene <nathant@utm.edu>
2022-03-29 15:30:38 -0500

    function for symlinks

--- find_user_videos.py
+++ find_user_videos.py
@@ -77,9 +77,7 @@ def create_symlink_to_files(files, username):
         directory = x['filename']
         #os.symlink(avideo_root_videos + directory, os.path.join(download_folder, os.path.dirname(avideo_root_videos)))
         new_path = download_folder + username + "/" + title + directory
-        isExist = os.path.exists(new_path)
-        if not isExist:
-            os.makedirs(new_path)
+        os.makedirs(new_path, exist_ok=True)
         os.symlink(avideo_root_videos + directory, new_path)
 
 
