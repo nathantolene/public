a76841e5b020686187d440972854d58392964ab2
Nathan Tolene <nathant@utm.edu>
2022-07-22 14:46:14 -0500

    testing

--- cisco.py
+++ cisco.py
@@ -39,18 +39,18 @@ def join_call(building, room, sip_number, passcode):
 def get_current_sip_number(building, room):
     string = '/Status/Call'
     gather = starter + building + room + domain + get_path + string
-    print(gather + string)
+    #print(gather + string)
     response = requests.get(gather, auth=(cisco_user, cisco_pass))
-    print(response.content)
+    #print(response.content)
     data = response.content
     converted_root = xmltodict.parse(data)
-    print(converted_root)
+    #print(converted_root)
     try:
         CallbackNumber = converted_root['Status']['Call']['CallbackNumber']
-        print(CallbackNumber)
+        #print(CallbackNumber)
         return CallbackNumber
     except KeyError:
-        syslog.syslog(syslog.LOG_ALERT, building + room + " Not in a call")
+        syslog.syslog(building + room + " Not in a call")
         return None
 
 
@@ -64,13 +64,12 @@ def disconnect_from_current_call(building, room, call_id):
     #xCommand Call Disconnect [CallId: CallId]
     xml_string = tostring(E.Command(E.Call(E.Disconnect(E.CallId(call_id)))),
                           pretty_print=True, xml_declaration=True, encoding='utf-8')
-    print(xml_string)
+    #print(xml_string)
     gather = starter + building + room + domain + post_path
     post = requests.post(gather, data=xml_string, auth=(cisco_user, cisco_pass))
     syslog.syslog("Disconnecting " + building + ' ' + room)
     # print(post.content)
     return post
-    pass
 
 
 def get_call_id(building, room):
