396ae50c0b615c83fca14dbf5b18c90de5b67547
Nathan Tolene <nathant@utm.edu>
2022-07-22 14:51:01 -0500

    testing

--- cisco.py
+++ cisco.py
@@ -62,14 +62,18 @@ def get_bookins_list(building, room):
 
 def disconnect_from_current_call(building, room, call_id):
     #xCommand Call Disconnect [CallId: CallId]
-    xml_string = tostring(E.Command(E.Call(E.Disconnect(E.CallId(call_id)))),
-                          pretty_print=True, xml_declaration=True, encoding='utf-8')
-    #print(xml_string)
-    gather = starter + building + room + domain + post_path
-    post = requests.post(gather, data=xml_string, auth=(cisco_user, cisco_pass))
-    syslog.syslog("Disconnecting " + building + ' ' + room)
-    # print(post.content)
-    return post
+    try:
+        xml_string = tostring(E.Command(E.Call(E.Disconnect(E.CallId(call_id)))),
+                              pretty_print=True, xml_declaration=True, encoding='utf-8')
+        # print(xml_string)
+        gather = starter + building + room + domain + post_path
+        post = requests.post(gather, data=xml_string, auth=(cisco_user, cisco_pass))
+        syslog.syslog("Disconnecting " + building + ' ' + room)
+        # print(post.content)
+        return post
+    except TypeError:
+        syslog.syslog(building + room + " Already Disconnected")
+
 
 
 def get_call_id(building, room):
