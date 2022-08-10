19e198c305615aec8050b6007714d5bb9fd2c43a
Nathan Tolene <nathant@utm.edu>
2022-08-10 15:02:42 -0500

    starting a new

--- google_cal_creator.py
+++ google_cal_creator.py
@@ -88,7 +88,7 @@ def find_duplicates():
 
 def get_classes(IDS):
     #print(IDS)
-    ID1 = IDS[0]
+    ID1 = str(IDS[0]['ID'])
     print(ID1)
     select_sql = "SELECT `SUBJ`, `CRS`,`TITLE`, `MTWRFS`, `TIME`, `INSTRUCTOR`, `SITE`, `Center Room #`, `Main Campus Rm #`, `Off Campus School`, `ID` FROM `importer` WHERE ID = '" + ID1 + "';"
     result = mysql_select(select_sql)
