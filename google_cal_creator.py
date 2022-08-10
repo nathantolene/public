2468df5dec3e7178b741c9bc35caf28cad3ede73
Nathan Tolene <nathant@utm.edu>
2022-08-10 14:38:23 -0500

    starting a new

--- google_cal_creator.py
+++ google_cal_creator.py
@@ -66,7 +66,7 @@ def find_duplicates():
             select_sql = "SELECT `SUBJ`, `CRS`,`TITLE`, `MTWRFS`, `TIME`, `INSTRUCTOR`, `SITE`, `Center Room #`, `Main Campus Rm #`, `Off Campus School`, `ID` FROM `importer` WHERE ID = '" + ID + "';"
             #print(select_sql)
             rooms = mysql_select(select_sql)
-            print(rooms[0])
+            #print(rooms)
             for z in rooms:
                 room = z['Center Room #']
                 site = z['SITE']
