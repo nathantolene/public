61aeccb4ec82b22e37092f82a04532b208b0e4bb
Nathan Tolene <nathant@utm.edu>
2022-08-14 10:44:56 -0500

    starting a new

--- google_cal_creator.py
+++ google_cal_creator.py
@@ -195,7 +195,7 @@ def zoom_info_add_attendees(location, row_id):
 def get_display_name_from_building_room(building, room):
     with open(r'room_info.yaml') as file:
         rooms = yaml.load(file, Loader=yaml.FullLoader)
-        #print(rooms)
+        print(rooms)
         for x in rooms:
             f_building = x['building']
             f_room = x['room']
