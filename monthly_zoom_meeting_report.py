46a7d5a042173a1dadd420788549272ee8da09f5
nathantolene <nathan.tolene@gmail.com>
2023-01-16 11:43:18 -0600

    added date to db

--- monthly_zoom_meeting_report.py
+++ monthly_zoom_meeting_report.py
@@ -19,11 +19,11 @@ def get_monthly_zoom_meetings():
             topic = y['topic']
             topic = topic.replace("'", "")
             insert_meeting_info = "insert into zoom_reports (uuid, zoom_id, meeting_type, topic, user_name, user_email, " \
-                                  "start_time, end_time, duration, total_minutes, participants_count, source) values ('" \
+                                  "start_time, end_time, duration, total_minutes, participants_count, source, date) values ('" \
                                   + y['uuid'] + parser + str(y['id']) + parser + str(y['type']) + parser + topic \
             + parser + y['user_name'] + parser + y['user_email'] + parser + y['start_time'] + parser + y['end_time'] + \
             parser + str(y['duration']) + parser + str(y['total_minutes']) + parser + str(y['participants_count']) \
-            + parser + y['source'] + "')"
+            + parser + y['source'] + parser + current_day + "')"
             # print(insert_meeting_info)
             database_maker.mysql_insert(insert_meeting_info)
 
