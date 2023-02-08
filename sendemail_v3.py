d786752489ac4e054dfc774abbaba4201709c75c
nathantolene <nathan.tolene@gmail.com>
2023-02-08 09:41:55 -0600

    wrong column name changed

--- sendemail_v3.py
+++ sendemail_v3.py
@@ -7,39 +7,83 @@ import syslog
 # from upload_to_avideo import get_status_of_video_from_avideo_db
 # from upload_to_avideo import delete_from_utm_videos_if_status_is_a
 # from upload_to_avideo import get_video_id_to_check_status
-import sys
+# import sys
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-host = os.environ.get('host')
-user = os.environ.get('dbuser')
-password = os.environ.get('dbpass')
-database = os.environ.get('db')
-avideo_host = os.environ.get('avideo_host')
-avideo_user = os.environ.get('avideo_dbuser')
-avideo_pass = os.environ.get('avideo_dbpass')
-avideo_database = os.environ.get('avideo_db')
-smtp_server = os.environ.get('smtp_server')
-from_address = os.environ.get('from_address')
-copy_nathan = os.environ.get('copy_nathan')
-location = os.environ.get('location')
-video_name = ''
-class_name = ''
-video_link = ''
-video_duration = ''
-video_description = ''
-cat_id = ''
-parser = '"'
+UTM_HOST = os.environ.get('host')
+UTM_USER = os.environ.get('dbuser')
+UTM_PASS = os.environ.get('dbpass')
+UTM_DB = os.environ.get('db')
+AV_HOST = os.environ.get('avideo_host')
+AV_USER = os.environ.get('avideo_dbuser')
+AV_PASS = os.environ.get('avideo_dbpass')
+AV_DB = os.environ.get('avideo_db')
+SMTP_SERVER = os.environ.get('smtp_server')
+FROM_ADDRESS = os.environ.get('from_address')
+COPY_ADMIN = os.environ.get('copy_nathan')
+LOCATION = os.environ.get('location')
+# video_name = ''
+# class_name = ''
+# video_link = ''
+# video_duration = ''
+# video_description = ''
+# cat_id = ''
+# parser = '"'
+
+
+class UtmAddOnDB:
+    class Videos:
+        def __init__(self, x):
+            self.id = x['id']
+            self.av_id = x['av_id']
+            self.status = x['status']
+            self.categories_id = x['categories_id']
+
+    class Email:
+        def __init__(self, x):
+            self.id = x['ID']
+            self.name = x['name']
+            self.email = x['email']
+            self.category = x['cat']
+            self.one_off = x['one_off']
+
+    class SendEmail:
+        def __init__(self, x):
+            self.id = x['id']
+            self.send_to = x['send_to']
+            self.subject = x['subject']
+            self.body = x['body']
+
+    class OffGroup:
+        def __init__(self, x):
+            self.id = x['id']
+            self.title = x['title']
+            self.off_group = '3'
+
+
+class AVideoDB:
+    class Videos:
+        def __init__(self, x):
+            self.name = x['title']
+            self.description = x['description']
+            self.duration = x['duration']
+            self.categories_id = x['categories_id']
+            self.class_name = x['title'].split(" ")
+            self.class_name = f"{self.class_name[0]} {self.class_name[1]} {self.class_name[2]}"
+            self.domain = 'https://dlcontent.utm.edu/v/'
+            self.channel = '?channelName=Upload'
+            self.link = ""
 
 
 def mysql_insert_update_utm(select_sql):
     db = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
+        host=UTM_HOST,
+        user=UTM_USER,
+        password=UTM_PASS,
+        database=UTM_DB
     )
     cursor = db.cursor(dictionary=True)
     cursor.execute(select_sql)
@@ -49,10 +93,10 @@ def mysql_insert_update_utm(select_sql):
 
 def mysql_select_utm(select_sql):
     db = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
+        host=UTM_HOST,
+        user=UTM_USER,
+        password=UTM_PASS,
+        database=UTM_DB
     )
     cursor = db.cursor(dictionary=True)
     cursor.execute(select_sql)
@@ -63,10 +107,10 @@ def mysql_select_utm(select_sql):
 
 def mysql_insert_update_avideo(select_sql):
     db = mysql.connector.connect(
-        host=avideo_host,
-        user=avideo_user,
-        password=avideo_pass,
-        database=avideo_database
+        host=AV_HOST,
+        user=AV_USER,
+        password=AV_PASS,
+        database=AV_DB
     )
     cursor = db.cursor(dictionary=True)
     cursor.execute(select_sql)
@@ -76,10 +120,10 @@ def mysql_insert_update_avideo(select_sql):
 
 def mysql_select_avideo(select_sql):
     db = mysql.connector.connect(
-        host=avideo_host,
-        user=avideo_user,
-        password=avideo_pass,
-        database=avideo_database
+        host=AV_HOST,
+        user=AV_USER,
+        password=AV_PASS,
+        database=AV_DB
     )
     cursor = db.cursor(dictionary=True)
     cursor.execute(select_sql)
@@ -89,72 +133,84 @@ def mysql_select_avideo(select_sql):
 
 
 def email_from_db():
-    class SendEmail:
-        def __init__(self, x):
-            self.id = x['id']
-            self.send_to = x['send_to']
-            self.subject = x['subject']
-            self.body = x['body']
-
-    mydb = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    mycursor = mydb.cursor(dictionary=True)
+    # mydb = mysql.connector.connect(
+    #     host=host,
+    #     user=user,
+    #     password=password,
+    #     database=database
+    # )
+    # mycursor = mydb.cursor(dictionary=True)
     select_sql = "select * from send_email"
-    mycursor.execute(select_sql)
-    result = mycursor.fetchall()
+    # mycursor.execute(select_sql)
+    # result = mycursor.fetchall()
+    result = mysql_select_utm(select_sql)
     for x in result:
-        email = SendEmail(x)
+        email = UtmAddOnDB.SendEmail(x)
         send_email = f'sendemail ' \
-                     f'-f {from_address} ' \
+                     f'-f {FROM_ADDRESS} ' \
                      f'-t {email.send_to} ' \
                      f'-u "{email.subject}" ' \
                      f'-m "{email.body}" ' \
-                     f'-s {smtp_server}'
+                     f'-s {SMTP_SERVER}'
         syslog.syslog(send_email)
         os.system(send_email)
-        select_sql = f"delete from send_email where id = '{str(email.id)}'"
-        mycursor.execute(select_sql)
-        mydb.commit()
-        mydb.close()
+        delete_sql = f"delete from send_email where id = '{str(email.id)}'"
+        mysql_insert_update_utm(delete_sql)
+        # mycursor.execute(select_sql)
+        # mydb.commit()
+        # mydb.close()
 
 
 def find_files_to_email():
-    # location = "/Users/nathantolene/email" #add to .env
-    for file in os.listdir(location):
+    for file in os.listdir(LOCATION):
         if file.endswith(".yaml"):
-            location_file = os.path.join(location, file)
+            location_file = os.path.join(LOCATION, file)
             with open(location_file, 'r') as yaml_file:
                 email = yaml.load(yaml_file, Loader=yaml.FullLoader)
                 # print(email['body'])
                 body = email['body']
                 send_to_address = email['send_to_address']
                 subject = email['subject']
-                send_email = 'sendemail -f ' + from_address + \
-                             ' -t ' + send_to_address + \
-                             ' -u ' + parser + subject + parser + \
-                             ' -m ' + parser + body + parser + ' -s ' + smtp_server
-                # print(send_email)
+                # send_email = 'sendemail -f ' + from_address + \
+                #              ' -t ' + send_to_address + \
+                #              ' -u ' + parser + subject + parser + \
+                #              ' -m ' + parser + body + parser + ' -s ' + smtp_server
+                send_email = f"sendemail" \
+                             f" -f {FROM_ADDRESS}" \
+                             f" -t {send_to_address}" \
+                             f" -u '{subject}'" \
+                             f" -m '{body}'" \
+                             f" -s {SMTP_SERVER}'"
+                if COPY_ADMIN == 'True':
+                    send_email = f"{send_email} -bcc nathant@utm.edu"
                 syslog.syslog(send_email)
                 os.system(send_email)
                 os.remove(location_file)
 
 
-def sendit(send_to_address, name):
-    statement = 'sendemail -f ' + from_address + \
-                ' -t ' + send_to_address + \
-                ' -u ' + parser + video_name + parser + \
-                ' -m ' + parser + "Dear " + name + "," + \
-                "\nA new video was uploaded to dlcontent.utm.edu server for " + class_name + ".\n\n" \
-                + "Video Name: " + video_name + '\n' \
-                + "Video Link: " + video_link + \
-                "\nVideo Duration: " + video_duration + \
-                "\nVideo Description: " + video_description + \
-                "\n\nThanks,\n\nUTM Distance Learning Content Server" + \
-                parser + ' -s ' + smtp_server
+def sendit(email, video):
+    # statement = 'sendemail -f ' + from_address + \
+    #             ' -t ' + send_to_address + \
+    #             ' -u ' + parser + video_name + parser + \
+    #             ' -m ' + parser + "Dear " + name + "," + \
+    #             "\nA new video was uploaded to dlcontent.utm.edu server for " + class_name + ".\n\n" \
+    #             + "Video Name: " + video_name + '\n' \
+    #             + "Video Link: " + video_link + \
+    #             "\nVideo Duration: " + video_duration + \
+    #             "\nVideo Description: " + video_description + \
+    #             "\n\nThanks,\n\nUTM Distance Learning Content Server" + \
+    #             parser + ' -s ' + smtp_server
+    body = f"Dear {email.name},\n" \
+           f"A new video was uploaded to dlcontent.utm.edu server for {video.class_name}.\n\n" \
+           f"Video Name: {video.name}\n" \
+           f"Video Link: {video.link}\n" \
+           f"Video Duration: {video.duration}\n" \
+           f"\nThanks,\n" \
+           f"\nUTM Distance Learning Content Server"
+    statement = f"sendemail -f {FROM_ADDRESS} -t {email.email} -u '{video.name}' " \
+                f"-m '{body}' -s {SMTP_SERVER}"
+    if COPY_ADMIN == 'True':
+        statement = f"{statement} -bcc nathant@utm.edu"
     syslog.syslog(statement)
     # print(statement)
     os.system(statement)
@@ -162,92 +218,109 @@ def sendit(send_to_address, name):
 
 def delete_one_off(email_id):
     email_id = str(email_id)
-    mydb = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    mycursor = mydb.cursor()
-    select_sql = "delete from email where ID = '" + email_id + "'"
-    mycursor.execute(select_sql)
-    mydb.commit()
+    # mydb = mysql.connector.connect(
+    #     host=host,
+    #     user=user,
+    #     password=password,
+    #     database=database
+    # )
+    # mycursor = mydb.cursor()
+    delete_sql = f"delete from email where ID = '{email_id}'"
+    mysql_insert_update_utm(delete_sql)
+    # mycursor.execute(select_sql)
+    # mydb.commit()
 
 
-def check_db_for_email_address(cat_id):
-    cat_id = str(cat_id)
-    mydb = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    mycursor = mydb.cursor(dictionary=True)
-    mycursor.execute("select * from email where cat = '" + cat_id + "'")
-    email_address = mycursor.fetchall()
+def check_db_for_email_address(video):
+    # cat_id = str(cat_id)
+    # mydb = mysql.connector.connect(
+    #     host=host,
+    #     user=user,
+    #     password=password,
+    #     database=database
+    # )
+    # mycursor = mydb.cursor(dictionary=True)
+    select_sql = f"select * from email where cat = '{video.categories_id}'"
+    email_address = mysql_select_utm(select_sql)
+    # mycursor.execute("select * from email where cat = '" + cat_id + "'")
+    # email_address = mycursor.fetchall()
     for x in email_address:
-        check_cat_id = x['cat']
-        address_to_send = x['email']
-        name_to_email = x['name']
-        one_off = x['one_off']
-        email_id = x['ID']
+        email = UtmAddOnDB.Email(x)
+        # check_cat_id = x['cat']
+        # address_to_send = x['email']
+        # name_to_email = x['name']
+        # one_off = x['one_off']
+        # email_id = x['ID']
         # print(email_id)
         # print(one_off)
-        if check_cat_id == cat_id:
-            syslog.syslog('Email Address: ' + address_to_send)
+        # if check_cat_id == cat_id:
+        if email.category == video.categories_id:
+            syslog.syslog(f'Email Address: {email.email}')
             # print(row[2])
-            sendit(address_to_send, name_to_email)
-            if one_off == '1':
-                delete_one_off(email_id)
-    if copy_nathan == 'True':
-        sendit('nathant@utm.edu', 'Automater')
+            sendit(email, video)
+            if email.one_off == '1':
+                delete_one_off(email.id)
+    # if copy_nathan == 'True':
+    #     sendit('nathant@utm.edu', 'Automater')
 
 
 def check_db_for_ready_videos():
     get_video_id_to_check_status()
-    mydb = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-    )
-    mycursor = mydb.cursor(dictionary=True)
-    select_sql = "select av_id from videos where status = 'a'"
-    mycursor.execute(select_sql)
-    ready_to_send = mycursor.fetchall()
+    # mydb = mysql.connector.connect(
+    #     host=host,
+    #     user=user,
+    #     password=password,
+    #     database=database
+    # )
+    # mycursor = mydb.cursor(dictionary=True)
+    # select_sql = "select av_id from videos where status = 'a'"
+    select_sql = "select * from videos where status = 'a'"
+    ready_to_send = mysql_select_utm(select_sql)
+    # mycursor.execute(select_sql)
+    # ready_to_send = mycursor.fetchall()
     for row in ready_to_send:
-        # print(row['av_id'])
-        av_id = row['av_id']
-        get_video_info_for_email(av_id)
-        delete_from_utm_videos_if_status_is_a(av_id)
+        video = UtmAddOnDB.Videos(row)
+        # av_id = row['av_id']
+        # av_id = video.av_id
+        get_video_info_for_email(video.av_id)
+        delete_from_utm_videos_if_status_is_a(video.av_id)
 
 
 def get_video_info_for_email(av_id):
-    mydb = mysql.connector.connect(
-        host=avideo_host,
-        user=avideo_user,
-        password=avideo_pass,
-        database=avideo_database
-    )
-    mycursor = mydb.cursor(dictionary=True)
+    # mydb = mysql.connector.connect(
+    #     host=avideo_host,
+    #     user=avideo_user,
+    #     password=avideo_pass,
+    #     database=avideo_database
+    # )
+    # mycursor = mydb.cursor(dictionary=True)
     av_id = str(av_id)
-    # https://dlcontent.utm.edu/v/2129?channelName=Upload
-    # select title, description, duration from videos where id = '2120
-    select_sql = "select title, description, duration, categories_id from videos where id = '" + av_id + "'"
-    mycursor.execute(select_sql)
-    info = mycursor.fetchall()
+    select_sql = f"select title, description, duration, categories_id from videos where id = '{av_id}'"
+    # mycursor.execute(select_sql)
+    info = mysql_select_avideo(select_sql)
+    # info = mycursor.fetchall()
     for row in info:
-        global video_name, video_description, video_duration, class_name, video_link, cat_id
-        video_name = row['title']
-        video_description = row['description']
-        video_duration = row['duration']
-        class_name = row['title'].split(" ")
-        class_name = class_name[0] + " " + class_name[1] + " " + class_name[2]
-        domain = 'https://dlcontent.utm.edu/v/'
-        channel = '?channelName=Upload'
-        video_link = domain + av_id + channel
-        cat_id = row['categories_id']
-        check_db_for_email_address(cat_id)
+        video = AVideoDB.Videos(row)
+        # global video_name, video_description, video_duration, class_name, video_link, cat_id
+        # video_name = row['title']
+        # video_name = video.name
+        # video_description = row['description']
+        # video_description = video.description
+        # video_duration = row['duration']
+        # video_duration = video.duration
+        # class_name = row['title'].split(" ")
+        # class_name = class_name[0] + " " + class_name[1] + " " + class_name[2]
+        # class_name = video.class_name
+        # domain = 'https://dlcontent.utm.edu/v/'
+        # domain = video.domain
+        # channel = '?channelName=Upload'
+        # channel = video.channel
+        # video_link = domain + av_id + channel
+        video.link = f"{video.domain}{av_id}{video.channel}"
+        # video_link = f"{domain}{av_id}{channel}"
+        # cat_id = row['categories_id']
+        # cat_id = video.categories_id
+        check_db_for_email_address(video)
 
 
 def delete_from_utm_videos_if_status_is_a(av_id):
@@ -309,14 +382,15 @@ def get_cat_name(cat_id):
 
 
 def find_off_videos_list(cat_name):
-    print(cat_name)
+    # print(cat_name)
     # cat_name = cat_name[0]['name']
     select_sql = "select * from off_group"
     result = mysql_select_utm(select_sql)
     for x in result:
-        title = x['title']
-        print(title)
-        if cat_name == title:
+        # title = x['title']
+        off_group = UtmAddOnDB.OffGroup(x)
+        # print(title)
+        if cat_name == off_group.title:
             return True
     return False
 
@@ -324,8 +398,9 @@ def find_off_videos_list(cat_name):
 def add_video_to_off_group(video_id):
     off_group = '3'
     video_id = str(video_id)
-    # insert_sql = "insert into videos_group_view (users_groups_id, videos_id) values (" + off_group + ", " + video_id + ")"
-    insert_sql = f"insert into videos_group_view (users_group_id, videos_id) values ('{off_group}', '{video_id}')"
+    # insert_sql =
+    # "insert into videos_group_view (users_groups_id, videos_id) values (" + off_group + ", " + video_id + ")"
+    insert_sql = f"insert into videos_group_view (users_groups_id, videos_id) values ('{off_group}', '{video_id}')"
     mysql_insert_update_avideo(insert_sql)
     # syslog(response)
 
