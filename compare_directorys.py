b869a1544cad06cef61e7550a9ff442990e1c952
nathantolene <88114426+nathantolene@users.noreply.github.com>
2025-02-05 15:26:55 -0600

    fixing videos test

--- compare_directorys.py
+++ compare_directorys.py
@@ -10,20 +10,20 @@ def main():
     shared_screen_path = path1
     # active_speaker_path = input('Path to active speaker')
     active_speaker_path = path2
-    print(shared_screen_path)
-    print(active_speaker_path)
+    # print(shared_screen_path)
+    # print(active_speaker_path)
     shared_screen_files = os.listdir(shared_screen_path)
     active_speaker_files = os.listdir(active_speaker_path)
-    print(shared_screen_files)
-    print(active_speaker_files)
+    # print(shared_screen_files)
+    # print(active_speaker_files)
     needs_moved = list(set(active_speaker_files) - set(shared_screen_files))
     for x in needs_moved:
         print(x)
         active_speaker_path = active_speaker_path.replace(" ", "\ ")
         active_speaker_path = f'{active_speaker_path}/{x}/'
         # shared_screen_path = shared_screen_path.replace(" ", "\ ")
-        print(f'{active_speaker_path}/{x}')
-        print(f'{shared_screen_path}/')
+        print(f'{active_speaker_path}')
+        print(f'{shared_screen_path}')
         print(f'cp "{active_speaker_path}" "{shared_screen_path}/"')
         # os.system(f'cp "{active_speaker_path}/{x}" "{shared_screen_path}/"')
         # os.system(f'cp {active_speaker_path}/{x} {shared_screen_path}')
