730a9829b1781fa3987f9c6b237e0e7ec17a4718
nathantolene <88114426+nathantolene@users.noreply.github.com>
2025-02-05 15:38:20 -0600

    fixing videos test

--- compare_directorys.py
+++ compare_directorys.py
@@ -7,21 +7,23 @@ path2 = '/smb/Zoom/downloads/active_speaker/ACCT 305 Hatch'
 
 def main():
     # shared_screen_path = input('Path to shared_screen')
-    shared_screen_path = path1
+    shared_screen_path_set = path1
     # active_speaker_path = input('Path to active speaker')
-    active_speaker_path = path2
+    active_speaker_path_set = path2
     # print(shared_screen_path)
     # print(active_speaker_path)
-    shared_screen_files = os.listdir(shared_screen_path)
-    active_speaker_files = os.listdir(active_speaker_path)
+    shared_screen_files = os.listdir(shared_screen_path_set)
+    active_speaker_files = os.listdir(active_speaker_path_set)
     # print(shared_screen_files)
     # print(active_speaker_files)
     needs_moved = list(set(active_speaker_files) - set(shared_screen_files))
     for x in needs_moved:
         print(x)
+        active_speaker_path = active_speaker_path_set
         # active_speaker_path = active_speaker_path.replace(" ", "\\ ")
         active_speaker_path = f'{active_speaker_path}/{x}'
         # shared_screen_path = shared_screen_path.replace(" ", "\ ")
+        shared_screen_path = shared_screen_path_set
         shared_screen_path = f'{shared_screen_path}/{x}'
         print(f'{active_speaker_path}')
         print(f'{shared_screen_path}')
